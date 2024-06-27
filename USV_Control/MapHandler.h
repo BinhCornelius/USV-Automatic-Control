595c2bbe03398794d03e9edf65133272ef7454ee
Hung - Binh Ha <hhbmanucians@gmail.com>
2024-06-27 14:22:21 +0700

    Update MapHandler.h

--- USV_Control/MapHandler.h
+++ USV_Control/MapHandler.h
@@ -13,7 +13,6 @@ public:
     explicit MapHandler(QObject *parent = nullptr);
     Q_INVOKABLE void addCoordinate(double latitude, double longitude);
     Q_INVOKABLE void sendCoordinates(double pathType, double rX, double rY, double omgX, double omgY);
-    //Q_INVOKABLE void requestGpsCoordinate();
     Q_INVOKABLE void sendPwm(int pwmValue1, int pwmValue2);
 
 signals:
